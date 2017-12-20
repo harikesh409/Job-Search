90e606760bb39105807cc7f6e9053fd2c659af25
p.harikesh <p.harikesh409@gmail.com>
2017-12-20 23:11:08 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,70 +56,12 @@ var email = document.getElementById('email').value;
 var password = document.getElementById('password1').value;
 var fname = $("#username1").val();
 var jtype = [];
-$(function() {
-  $('#login-form-link').click(function(e) {
-    $("#login-form").delay(100).fadeIn(100);
-    $("#register-form").fadeOut(100);
-    $('#register-form-link').removeClass('active');
-    $(this).addClass('active');
-    $('#lg').addClass('active');
-    $('#rg').removeClass('active');
-    e.preventDefault();
-  });
-$('#register-form-link').click(function(e) {
-  $("#register-form").delay(100).fadeIn(100);
-  $("#login-form").fadeOut(100);
-  $('#login-form-link').removeClass('active');
-  $(this).addClass('active');
-  $('#lg').removeClass('active');
-  $('#rg').addClass('active');
-  e.preventDefault();
+$.each($('#jtype :checkbox:checked'),function () {
+  jtype.push($(this).val());
 });
-$('#register-submit').click(function(e) {
-  var p1 = $("#password1").val();
-  var p2 = $("#confirm-password").val();
-  var email = $("#email").val();
-  var fname = $("#username1").val();
-  if(fname == "" ) {
-    $("#err").html("Name can't be empty");
-    // $("#username1").addClass("ebdr");
-    e.preventDefault();
-  } else if (!validateEmail(email)) {
-    $("#err").html("Enter a valid email");
-    // $("#register-form .form-control").removeClass("ebdr");
-    e.preventDefault();
-  } else if (p1 == "") {
-    $("#err").html("Password can't be empty");
-    e.preventDefault();
-  } else if (p2 == "") {
-    $("#err").html("Confirm Password can't be empty");
-    e.preventDefault();
-  } else if(p1 != p2) {
-    $("#err").html("Enter same passwords!!");
-    e.preventDefault();
-  } else if (p1.length < 4) {
-    $("#err").html('Password length must be atleast 4 characters');
-    e.preventDefault();
-  } else if ($('#jtype :checkbox:checked').length <= 0) {
-    $("#err").html("Choose atleast 1 type of Job");
-    e.preventDefault();
-  } else {
-    handleSignUp();
-  }
-
-});
-});
-function handleSignUp() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password1').value;
-    var fname = $("#username1").val();
-    var jtype = [];
-    $.each($('#jtype :checkbox:checked'),function () {
-      jtype.push($(this).val());
-    });
 // console.log(jtype);
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then(function(firebaseuser) {
+firebase.auth().createUserWithEmailAndPassword(email, password)
+.then(function(firebaseuser) {
   var databaseRef = firebase.database().ref().child('users/' + firebaseuser.uid);
   databaseRef.set({
     displayName: fname,
@@ -146,7 +88,7 @@ function handleSignUp() {
   });
 
   // setTimeout(function(){
-  //   window.location.href = "index.html";
+  //   top.location.href = "index.html";
   // },3000);
 
 })
@@ -169,6 +111,7 @@ function toggleSignIn() {
 
 if (firebase.auth().currentUser) {
   // [START signout]
+
   firebase.auth().signOut();
   // [END signout]
 } else {
@@ -243,7 +186,7 @@ function googleSignIn() {
     var errorMessage = error.message;
     var email = error.email;
     var credential = error.credential;
-    alert(errorMessage);
+    console.log(errorMessage);
   });
 } else {
   firebase.auth().signOut();
@@ -269,7 +212,7 @@ function fbSignIn() {
       var errorMessage = error.message;
       var email = error.email;
       var credential = error.credential;
-      alert(errorMessage);
+      console.log(errorMessage);
     });
   }
   else {
