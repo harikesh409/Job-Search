efa7e53a62897ab642d74e7ff4754f02dbb3ed0f
p.harikesh <p.harikesh409@gmail.com>
2017-12-20 23:03:34 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -141,7 +141,7 @@ function handleSignUp() {
   // $("#login-form-link").click();
   firebase.auth().onAuthStateChanged(function(user) {
     if(user) {
-      window.location.href = "index.html";
+      top.location.href = "index.html";
     }
   });
 
@@ -191,11 +191,11 @@ if (firebase.auth().currentUser) {
       if (user && user.emailVerified) {
         window.sessionStorage.setItem("UID",user.uid);
         window.sessionStorage.setItem("email",user.email);
-        window.location = 'viewProfile.html';
+        top.location.href = 'viewProfile.html';
       } else {
         window.sessionStorage.setItem("UID",user.uid);
         window.sessionStorage.setItem("email",user.email);
-        window.location = 'verify.html';
+        top.location.href = 'verify.html';
         alert("Please verify your email address!");
       }
     });
@@ -237,12 +237,13 @@ function googleSignIn() {
     var user = result.user;
     window.sessionStorage.setItem("UID",user.uid);
     window.sessionStorage.setItem("email",user.email);
-    window.location = 'viewProfile.html';
+    top.location.href = 'viewProfile.html';
   }).catch(function(error){
     var errorCode = error.code;
     var errorMessage = error.message;
     var email = error.email;
     var credential = error.credential;
+    alert(errorMessage);
   });
 } else {
   firebase.auth().signOut();
@@ -262,173 +263,13 @@ function fbSignIn() {
       var user = result.user;
       window.sessionStorage.setItem("UID",firebase.auth().currentUser.uid);
       window.sessionStorage.setItem("email",user.email);
-      window.location = 'viewProfile.html';
+      top.location.href = 'viewProfile.html';
     }).catch(function(error){
       var errorCode = error.code;
       var errorMessage = error.message;
       var email = error.email;
       var credential = error.credential;
-    });
-  }
-  else {
-    firebase.auth().signOut();
-  }
-}
-
-$.each($('#jtype :checkbox:checked'),function () {
-  jtype.push($(this).val());
-});
-// console.log(jtype);
-firebase.auth().createUserWithEmailAndPassword(email, password)
-.then(function(firebaseuser) {
-  var databaseRef = firebase.database().ref().child('users/' + firebaseuser.uid);
-  databaseRef.set({
-    displayName: fname,
-    email: email,
-    pass: password,
-    jType: jtype
-  });
-  var picurl = 'https://pikmail.herokuapp.com/'+email+'?size=150'
-  firebase.auth().currentUser.updateProfile({
-    displayName:fname,
-    photoURL:[picurl]
-  });
-  firebase.auth().currentUser.sendEmailVerification().then(function(){
-    alert("Verification mail sent kindly verify it");
-  }).catch(function(error){
-    console.log("Error");
-  });
-  // alert("Account created you can login");
-  // $("#login-form-link").click();
-  firebase.auth().onAuthStateChanged(function(user) {
-    if(user) {
-      window.location.href = "index.html";
-    }
-  });
-
-  // setTimeout(function(){
-  //   window.location.href = "index.html";
-  // },3000);
-
-})
-.catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-
-  if (errorCode == 'auth/user-not-found') {
-    $("#err").html('User already present');
-  } else {
-    $("#err").html(errorMessage);
-  }
-  console.log(error);
-
-});
-// [END createwithemail]
-}
-function toggleSignIn() {
-  
-if (firebase.auth().currentUser) {
-  // [START signout]
-  firebase.auth().signOut();
-  // [END signout]
-} else {
-  var email = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  if (!validateEmail(email)) {
-    $("#err").html('Please enter a valid email address.');
-    return;
-  }
-  if (password.length < 4) {
-    $("#err").html('Please enter a valid password.');
-    return;
-  }
-  // Sign in with email and pass.
-  // [START authwithemail]
-  firebase.auth().signInWithEmailAndPassword(email, password)
-  .then(function(firebaseuser) {
-    // alert(firebaseuser.uid);
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user && user.emailVerified) {
-        window.sessionStorage.setItem("UID",user.uid);
-        window.sessionStorage.setItem("email",user.email);
-        window.location = 'profile.html';
-      } else {
-        window.sessionStorage.setItem("UID",user.uid);
-        window.sessionStorage.setItem("email",user.email);
-        window.location = 'verify.html';
-        alert("Please verify your email address!");
-      }
-    });
-  })
-  .catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode === 'auth/wrong-password') {
-      $("#err").html('Wrong password.');
-    } else if (errorCode === 'auth/user-not-found') {
-      $("#err").html("User not found");
-    } else {
       alert(errorMessage);
-    }
-    console.log(error);
-    // [END_EXCLUDE]
-  });
-  // [END authwithemail]
-
-}
-}
-function validateEmail(sEmail) {
-var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-if (filter.test(sEmail)) {
-  return true;
-}
-else {
-  return false;
-}
-}
-function googleSignIn() {
-  if (!firebase.auth().currentUser) {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-  firebase.auth().signInWithPopup(provider).then(function(result){
-    var token = result.credential.accessToken;
-    var user = result.user;
-    window.sessionStorage.setItem("UID",user.uid);
-    window.sessionStorage.setItem("email",user.email);
-    window.location = 'profile.html';
-  }).catch(function(error){
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-  });
-} else {
-  firebase.auth().signOut();
-}
-}
-function fbSignIn() {
-  if (!firebase.auth().currentUser) {
-    var provider = new firebase.auth.FacebookAuthProvider();
-    provider.addScope('email');
-    provider.addScope('public_profile');
-    provider.addScope('user_location');
-    provider.setCustomParameters({
-      'display': 'popup'
-    });
-    firebase.auth().signInWithPopup(provider).then(function(result){
-      var token = result.credential.accessToken;
-      var user = result.user;
-      window.sessionStorage.setItem("UID",firebase.auth().currentUser.uid);
-      window.sessionStorage.setItem("email",user.email);
-      window.location ='viewProfile.html';
-    }).catch(function(error){
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
     });
   }
   else {
